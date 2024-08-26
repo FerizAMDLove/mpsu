46024dcb57e809ee9fa554419a7081987961d2b6
FerizAMDLove <renusanddan4ik@mail.ru>
2024-08-26 20:04:56 +0300

    Add files via upload

---
+++ arithmetic_operation_4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+int a = 10;
+int b = 5;
+float sum = a + b;
+float average = sum / 2;
+printf("sum = %1.0f\n", sum);
+printf("average = %4.1f", average);
+return 0;
+}   
