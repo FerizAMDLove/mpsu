11f6c4454e190c4da9662ee8c5a12a9ce871aac8
FerizAMDLove <renusanddan4ik@mail.ru>
2024-08-27 21:30:38 +0300

    Add files via upload

---
+++ cycle_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) 
+{ 
+    for(int x=1;x<3;x++)
+    {
+printf("hello work\n");
+    }
+    int x = 1;
+    while(x<4)
+    {
+    printf("hello work\n");
+    x++;
+    }
+    return 0;
+}
