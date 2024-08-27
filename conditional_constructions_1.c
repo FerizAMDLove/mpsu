29b79ea547dfa56ff065d78ba850e11857f3e677
FerizAMDLove <renusanddan4ik@mail.ru>
2024-08-27 20:38:18 +0300

    Add files via upload

---
+++ conditional_constructions_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) 
+{ 
+    int x = 10;
+    int y = 5;
+    if(x == y)
+        printf("var equal");
+        else 
+        printf("var not equal");
+    
+    return 0;
+}
