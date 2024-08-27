bc45da6331f611b0f04552b75f852f90010f3fac
FerizAMDLove <renusanddan4ik@mail.ru>
2024-08-27 20:42:18 +0300

    Add files via upload

---
+++ conditional_constructions_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) 
+{ 
+    int x = 10;
+    if(x > 4 & x < 14)
+        printf("within range (4..14)");
+        else 
+        printf("out of range");
+    
+    return 0;
+}
